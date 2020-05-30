68e84449fb2abbba7692a99e988c963671643c6f
pAdre91 <petr.osipov91@mail.ru>
2020-05-30 17:51:42 +0700

    Добавлен метод определения нахождения всего объекта в видимой зоне камеры

--- RightWay_asteroids/Assets/Scripts/Gameplay/Helpers/GameAreaHelper.cs
+++ RightWay_asteroids/Assets/Scripts/Gameplay/Helpers/GameAreaHelper.cs
@@ -30,7 +30,24 @@ namespace Gameplay.Helpers
 				&& (objectPos.x + objectBounds.extents.x > leftBound)
 				&& (objectPos.y - objectBounds.extents.y < topBound)
 				&& (objectPos.y + objectBounds.extents.y > bottomBound);
+		}
 
+		public static bool IsAllObjectInGameplayArea(Transform objectTransform, Bounds objectBounds)
+		{
+			var camHalfHeight = _camera.orthographicSize;
+			var camHalfWidth = camHalfHeight * _camera.aspect;
+			var camPos = _camera.transform.position;
+			var topBound = camPos.y + camHalfHeight;
+			var bottomBound = camPos.y - camHalfHeight;
+			var leftBound = camPos.x - camHalfWidth;
+			var rightBound = camPos.x + camHalfWidth;
+
+			var objectPos = objectTransform.position;
+
+			return (objectPos.x < rightBound - objectBounds.extents.x)
+				&& (objectPos.x > leftBound + objectBounds.extents.x)
+				&& (objectPos.y < topBound - objectBounds.extents.y)
+				&& (objectPos.y > bottomBound + objectBounds.extents.y);
 		}
 
 	}
