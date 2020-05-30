3490f60dab973f3c25866acc0c7beba181c472c4
pAdre91 <petr.osipov91@mail.ru>
2020-05-30 19:42:41 +0700

    Добавление иновой пули и оружия

---
+++ RightWay_asteroids/Assets/Scripts/Gameplay/Weapons/Projectiles/CustomProjectiles/Rocket.cs
@@ -0,0 +1,10 @@
+﻿using Gameplay.Weapons.Projectiles;
+using UnityEngine;
+
+public class Rocket : Projectile
+{
+	protected override void Move(float speed)
+	{
+		transform.Translate(speed * Time.deltaTime * Vector3.up);
+	}
+}
