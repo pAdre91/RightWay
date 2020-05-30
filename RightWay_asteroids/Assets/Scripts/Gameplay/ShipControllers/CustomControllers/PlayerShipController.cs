3d01545e70974b3c77493733030862a496c75411
pAdre91 <petr.osipov91@mail.ru>
2020-05-30 17:52:42 +0700

    Обработка выхода игрока за границы экрана

--- RightWay_asteroids/Assets/Scripts/Gameplay/ShipControllers/CustomControllers/PlayerShipController.cs
+++ RightWay_asteroids/Assets/Scripts/Gameplay/ShipControllers/CustomControllers/PlayerShipController.cs
@@ -1,13 +1,22 @@
 ﻿using Gameplay.ShipSystems;
+using Gameplay.Helpers;
 using UnityEngine;
 
 namespace Gameplay.ShipControllers.CustomControllers
 {
 	public class PlayerShipController : ShipController
 	{
+		[SerializeField]
+		private Collider2D objectCollider;
+
 		protected override void ProcessHandling(MovementSystem movementSystem)
 		{
+			Vector3 oldPosition = gameObject.transform.position;
+
 			movementSystem.LateralMovement(Input.GetAxis("Horizontal") * Time.deltaTime);
+
+			if (!IsPlayerPositionInArea())
+				gameObject.transform.position = oldPosition;
 		}
 
 		protected override void ProcessFire(WeaponSystem fireSystem)
@@ -16,6 +25,13 @@ namespace Gameplay.ShipControllers.CustomControllers
 			{
 				fireSystem.TriggerFire();
 			}
+		}
+
+		private bool IsPlayerPositionInArea()
+		{
+			if (GameAreaHelper.IsAllObjectInGameplayArea(transform, objectCollider.bounds))
+				return true;
+			return false;
 		}
 	}
 }
