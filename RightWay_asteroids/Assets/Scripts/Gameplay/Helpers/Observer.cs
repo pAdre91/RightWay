df46e36dba8d5e45e6d3c47f1851b712136da1b7
pAdre91 <petr.osipov91@mail.ru>
2020-05-31 19:51:11 +0700

    Добавление кастомной системы событий

---
+++ RightWay_asteroids/Assets/Scripts/Gameplay/Helpers/Observer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine.Events;
+
+namespace Gameplay.Helpers
+{
+	public class Event<T> : UnityEvent<T>
+	{
+	}
+
+	public class Observer
+	{
+		private static Observer instanceHolder = null;
+
+		public static Observer Instance()
+		{
+			if (instanceHolder != null)
+				return instanceHolder;
+			return instanceHolder = new Observer();
+		}
+
+		public Event<float> EnemyDown;
+
+		Observer()
+		{
+			InitEvents();
+		}
+
+		public void InitEvents()
+		{
+			EnemyDown = new Event<float>();
+		}
+	}
+}
