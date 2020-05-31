89ec931d323311d7b5582ada4d66554aaff48a05
pAdre91 <petr.osipov91@mail.ru>
2020-05-31 21:42:52 +0700

    Включеие панели при смерти игрока

---
+++ RightWay_asteroids/Assets/Scripts/Gameplay/GUI/CustomPanels/LosePanel.cs
@@ -0,0 +1,34 @@
+﻿using Gameplay.Helpers;
+
+namespace Gameplay.GUI.CustomPanels
+{
+	public class LosePanel : EndGamePanel
+	{
+		private void Start()
+		{
+			Subscribe();
+			TurnOffPanel();
+		}
+
+		private void OnDestroy()
+		{
+			UnSubscribe();
+		}
+
+		private void RefreshPanel(float score)
+		{
+			TurnOnPanel();
+			SetInfo(score.ToString());
+		}
+
+		private void Subscribe()
+		{
+			Observer.Instance().PlayerDeadWithScore.AddListener(RefreshPanel);
+		}
+
+		private void UnSubscribe()
+		{
+			Observer.Instance().PlayerDeadWithScore.RemoveListener(RefreshPanel);
+		}
+	}
+}
