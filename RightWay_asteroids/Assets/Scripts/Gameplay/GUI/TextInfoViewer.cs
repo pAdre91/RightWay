f4e21c32e0e097527c7e5b90261a6abfd7c7247a
pAdre91 <petr.osipov91@mail.ru>
2020-05-31 15:02:56 +0700

    Добавление скрипта отображения текствой информации

---
+++ RightWay_asteroids/Assets/Scripts/Gameplay/GUI/TextInfoViewer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Gameplay.GUI
+{
+	public class TextInfoViewer : MonoBehaviour
+	{
+		[SerializeField]
+		private Text textField;
+
+		[SerializeField]
+		private string _prefix;
+
+		public void Display(string info)
+		{
+			textField.text = _prefix + info;
+		}
+	}
+}
