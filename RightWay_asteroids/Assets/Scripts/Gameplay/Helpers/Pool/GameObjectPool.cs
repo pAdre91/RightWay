e0a33a8bc39588caf4d8871091a8fcdbfd51c4ca
pAdre91 <petr.osipov91@mail.ru>
2020-06-02 00:28:41 +0700

    1) Добавлен пул объектов 2) Добавлено хранилище пулов

---
+++ RightWay_asteroids/Assets/Scripts/Gameplay/Helpers/Pool/GameObjectPool.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameplay.Helpers.Pool
+{
+	public class GameObjectPool
+	{
+		private Queue<GameObject> _objectPool = new Queue<GameObject>();
+		private GameObject _sampleObject;
+
+		public void GeneratePool(GameObject poolType, int count)
+		{
+			_sampleObject = poolType;
+			for (int i = 0; i < count; i++)
+			{
+				GameObject newObject = GameObject.Instantiate(poolType);
+				newObject.SetActive(false);
+
+				_objectPool.Enqueue(newObject);
+			}
+		}
+
+		public void Release(GameObject element)
+		{
+			element.gameObject.SetActive(false);
+			_objectPool.Enqueue(element);
+		}
+
+		public GameObject Take()
+		{
+			int counter = 0;
+
+			while (true)
+			{
+				if (++counter == _objectPool.Count || _objectPool.Count == 0)
+				{
+					GameObject newObject = GameObject.Instantiate(_sampleObject);
+					_objectPool.Enqueue(newObject);
+					return newObject;
+				}
+
+				if (!_objectPool.Peek().activeSelf)
+					return _objectPool.Dequeue();
+
+				GameObject obj = _objectPool.Dequeue();
+				_objectPool.Enqueue(obj);
+			}
+		}
+	}
+}
---
+++ RightWay_asteroids/Assets/Scripts/Gameplay/Storage/ObjectsStorage.cs
@@ -0,0 +1,86 @@
+﻿#pragma warning disable CS0649
+
+using Gameplay.Helpers;
+using Gameplay.Helpers.Pool;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameplay.Storage
+{
+	public class ObjectsStorage : MonoBehaviour
+	{
+		[SerializeField]
+		private List<GameObject> _spawnObjects;
+
+		[SerializeField]
+		private int _defaultPoolsSize;
+
+		private Dictionary<string, GameObjectPool> _pools = new Dictionary<string, GameObjectPool>();
+		private List<string> _poolsKey = new List<string>();
+
+		public static ObjectsStorage Instance { get; private set; }
+
+		private void Awake()
+		{
+			if (Instance == null)
+				Instance = this;
+			else
+				Destroy(this);
+		}
+
+		private void Start()
+		{
+			InitializePools(_spawnObjects, _defaultPoolsSize);
+
+			Subscribe();
+		}
+
+		private void OnDestroy()
+		{
+			UnSubscribe();
+		}
+
+		public GameObject GetObject(string objectType)
+		{
+			if (!_poolsKey.Contains(objectType))
+				throw new System.NotImplementedException();
+			return _pools[objectType].Take();
+		}
+
+		private void InitializePools(List<GameObject> typesObject, int poolSize)
+		{
+			if (typesObject.Count == 0)
+				throw new System.NotImplementedException();
+
+			foreach (GameObject enemy in typesObject)
+			{
+				if (!enemy.TryGetComponent(out PooledObject pooledObject))
+					continue;
+
+				GameObjectPool pool = new GameObjectPool();
+				_pools.Add(pooledObject.Type, pool);
+				pool.GeneratePool(enemy, poolSize);
+				_poolsKey.Add(pooledObject.Type);
+			}
+		}
+
+		private void ReturnObjectToStorage(GameObject gameObject)
+		{
+			if (!gameObject.TryGetComponent(out PooledObject pooledObject))
+				return;
+
+			_pools[pooledObject.Type].Release(gameObject);
+		}
+
+		private void Subscribe()
+		{
+			Observer.Instance().ObectOutdated.AddListener(ReturnObjectToStorage);
+		}
+
+		private void UnSubscribe()
+		{
+			Observer.Instance().ObectOutdated.RemoveListener(ReturnObjectToStorage);
+		}
+	}
+}
